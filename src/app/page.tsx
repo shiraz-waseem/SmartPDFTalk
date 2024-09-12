3431a9db90a9c668a9a26cc89c9f2743276d1453
shirazwaseem <shiraz.waseem@octdaily.com>
2024-09-12 23:31:07 +0500

    Clerk Authentication Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 
+console.log("I am here ")
+
 export default function Home() {
   return (
     <Button>First Commit is Setup ShadCN</Button>
-    )
+  )
 }
